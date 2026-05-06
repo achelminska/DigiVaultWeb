02717a45458813566c6b6a9393abc90eec919b56
Aleksandra Chełmińska <aichelminska99@gmail.com>
2026-05-06 23:43:34 +0200

    feat: connect Settings view to API with commission update
    Closes #11

--- DigiVault.Intranet/Controllers/SettingsController.cs
+++ DigiVault.Intranet/Controllers/SettingsController.cs
@@ -1,12 +1,28 @@
+using DigiVault.Intranet.Models;
+using DigiVault.Intranet.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DigiVault.Intranet.Controllers;
 
 public class SettingsController : Controller
 {
-    // GET
-    public IActionResult Index()
+    private readonly ApiService _api;
+
+    public SettingsController(ApiService api)
     {
-        return View();
+        _api = api;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var settings = await _api.GetAsync<AdminSettingsDto>("api/admin/settings");
+        return View(settings);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Update(decimal commissionRate)
+    {
+        await _api.PutAsync("api/admin/settings/commission", new { CommissionRate = commissionRate / 100 });
+        return RedirectToAction(nameof(Index));
     }
 }
---
+++ DigiVault.Intranet/Models/AdminSettingsDto.cs
@@ -0,0 +1,7 @@
+namespace DigiVault.Intranet.Models;
+
+public class AdminSettingsDto
+{
+    public decimal CommissionRate { get; set; }
+    public decimal PlatformBalance { get; set; }
+}
